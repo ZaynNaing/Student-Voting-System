28788ae7e29ccd91fd2d541320640f1516df1b84
Zayn <znaing@miu.edu>
2024-12-11 14:24:50 -0600

    initial configuration

---
+++ back-end/svs/src/test/java/com/svs/svs/SvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svs.svs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
