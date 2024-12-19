a97bb3042ed75ba72c05c12a933ac4c9b1681294
kash <parakash.sbd@gmail.com>
2024-12-19 16:22:42 -0600

    feat: added entity

--- back-end/svs/src/main/java/com/svs/svs/entity/Vote.java
+++ back-end/svs/src/main/java/com/svs/svs/entity/Vote.java
@@ -1,4 +1,59 @@
 package com.svs.svs.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_votes")
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "policy_id", nullable = false)
+    private Policy policy;
+
+    public Vote() {
+    }
+
+    public Vote(String type, User user, Policy policy) {
+        this.type = type;
+        this.user = user;
+        this.policy = policy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Policy getPolicy() {
+        return policy;
+    }
+
+    public void setPolicy(Policy policy) {
+        this.policy = policy;
+    }
 }
